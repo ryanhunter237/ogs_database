7e4b7c3f333c2bfa8c65d480e5f408848c4f31c1
Ryan Hunter <ryanhunter237@gmail.com>
2025-10-07 06:23:16 +0000

    Use execute_values instead of execute_batch

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import os
 import psycopg2
-from psycopg2.extras import execute_batch
+from psycopg2.extras import execute_values
+from time import time
 
 
 from load import get_gamedata
 from gameproc import get_board_hashes_and_moves
 
-INSERT_BATCH_SIZE = 500
+INSERT_BATCH_SIZE = 5_000
 
 dsn = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/mydb")
 
@@ -33,10 +34,9 @@ def insert_batch(data: list[tuple]):
         with conn.cursor() as cur:
             insert_sql = """
             INSERT INTO moves (game_id, move_num, board_hash, move_x, move_y)
-            VALUES (%s, %s, %s, %s, %s)
-            ON CONFLICT DO NOTHING;
+            VALUES %s
             """
-            execute_batch(cur, insert_sql, data)
+            execute_values(cur, insert_sql, data)
 
 
 def get_moves_data(gamedata: dict, num_moves: int) -> list[tuple[int, int, bytes, int, int]]:
@@ -50,8 +50,9 @@ def get_moves_data(gamedata: dict, num_moves: int) -> list[tuple[int, int, bytes
 
 
 def run():
-    start = 100_000
-    stop = 200_000
+    start = 400_000
+    stop = 500_000
+    print(f"Processing {start}:{stop}")
     create_moves_table()
     with ProcessPoolExecutor(8) as executor:
         futures = [
@@ -70,4 +71,6 @@ def run():
             insert_batch(batch)
 
 if __name__ == "__main__":
+    t0 = time()
     run()
+    print(f"{time() - t0:.2f}")
